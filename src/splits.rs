cd40ebd7673b5f072515c6184762c765a39ce42d
jenirain <jenirainer@icloud.com>
2025-09-07 13:52:19 +0200

    Updated doctests comment.

--- src/splits.rs
+++ src/splits.rs
@@ -1,13 +1,16 @@
 /// Split on multiple delimiters - this will handle multiple delimiters,
 /// splitting the input string on any of the delimiters or a combination of them.
+/// This method uses lifetime parameters which means that the returned vector will have a lifetime
+/// that is the same as the input string.
+///
 /// # Example:
 /// ```
-/// use rust_strings::splits::split_on_any;
+/// use rust_strings::splits::split_on_delimiters;
 /// let input = "big,!a brown,cow!";
 /// let delimiters = [',','!','a',' '];
 /// let expected_output: Vec<&str> = ["big", "brown", "cow"].to_vec();
-/// assert_eq!(expected_output, split_on_any(&input, &delimiters));
-pub fn split_on_any<'a>(s: &'a str, delimiters: &[char]) -> Vec<&'a str> {
+/// assert_eq!(expected_output, split_on_delimiters(&input, &delimiters));
+pub fn split_on_delimiters<'a>(s: &'a str, delimiters: &[char]) -> Vec<&'a str> {
     let mut output = Vec::new();
     let mut start = 0;
     for (i, c) in s.char_indices() {
